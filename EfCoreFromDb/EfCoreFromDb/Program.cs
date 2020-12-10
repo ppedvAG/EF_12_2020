21273a879832caf8d8899bff5df88c60daf1e82b
André <ruland@outlook.de>
2020-12-10 16:58:20 +0100

    EFCore From Database

---
+++ EfCoreFromDb/EfCoreFromDb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreFromDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            using var context = new NORTHWNDContext();
+
+            //foreach (var emps in context.Employees.Include(x => x.Orders).ToList())
+            foreach (var emps in context.Employees.ToList())
+            {
+                Console.WriteLine($"{emps.LastName}");
+
+                //context.Entry(emps).Collection(x => x.Orders).Load(); //expl.
+                foreach (var o in emps.Orders)
+                {
+
+                    Console.WriteLine($"\t{o.OrderDate:d}");
+                }
+
+            }
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+
+        }
+    }
+}
