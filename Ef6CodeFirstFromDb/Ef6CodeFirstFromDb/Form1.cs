c05124d8fa0cb3ea123311e4e1657fa2d4481747
André <ruland@outlook.de>
2020-12-10 16:58:05 +0100

    EF6 Code First from Database

---
+++ Ef6CodeFirstFromDb/Ef6CodeFirstFromDb/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ef6CodeFirstFromDb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Model1 context = new Model1();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = context.Employees.ToList();
+        }
+    }
+}
